e2f548298b1e27688b0874d7a2a7914c50e345ad
vinicius.esufg <vinicius.esufg@gmail.com>
2016-09-05 18:19:34 -0300

    adding bridge configuration to make dnsmasq work inside container

--- installer.sh
+++ installer.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+CONTROL_NETWORK_IP="10.0.0.200"
+IP_BASE_DHCP_RANGE="10.0.0"
+
+install_dependencies() {
+    apt-get update
+    apt-get install -y --force-yes bridge-utils
+}
+
 install_docker() {
 
     if [ "$(ls -A /etc/apt/sources.list.d/docker.list)" ]; then
@@ -25,9 +33,53 @@ install_docker_compose() {
     chmod +x /usr/local/bin/docker-compose
 }
 
+set_service_interface() {
+    oldIFS=$IFS
+
+    while read line; do
+        IFS='=' read -r -a array <<< "$line"
+        if [[ ${array[0]} = *"control_network"* ]]; then
+            CONTROL_NETWORK_INTERFACE=${array[1]}
+        fi
+        IFS=$'n'
+    done < $INSTALLER_HOME/conf/interface-network-map.conf
+    IFS=$old_IFS
+}
+
+set_ips() {
+    set_service_interface
+
+    CONTROL_NETWORK_IP=$(/sbin/ifconfig $CONTROL_NETWORK_INTERFACE | grep 'inet end.:' | cut -d: -f2 | awk '{ print $1}')
+
+    if [ -z "$CONTROL_NETWORK_IP" -o "$CONTROL_NETWORK_IP" == " " ]; then
+        CONTROL_NETWORK_IP=$(/sbin/ifconfig $CONTROL_NETWORK_INTERFACE | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+    fi
+
+    IP_BASE_DHCP_RANGE=$(echo $CONTROL_NETWORK_IP | cut -d"." -f1-3)
+}
+
+configure_bridge() {
+
+    # Delete the IP address from eth1
+    ip addr del $CONTROL_NETWORK_IP/16 dev eth1
+
+    # Create "shared_nw" with a bridge name "docker1"
+    docker network create \
+        --driver bridge \
+        --subnet=$IP_BASE_DHCP_RANGE.1/16 \
+        --opt "com.docker.network.bridge.name"="docker_fibre" \
+        fibre_nw
+    # Add docker1 to eth1
+    brctl addif docker_fibre eth1
+}
+
 main() {
+    install_dependencies
+    configure_bridge
     install_docker
     install_docker_compose
 }
 
 main
+
+#docker run --name container1 --net fibre_nw --ip 10.0.0.200 -dt ubuntu
